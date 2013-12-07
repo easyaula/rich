4f9b121fdff015bf4cec1267ec74a4914600acfc
Victor Campos <victorcampos@ufrj.br>
2013-12-07 02:34:16 +0000

    adding rise to debug

--- app/inputs/rich_input.rb
+++ app/inputs/rich_input.rb
@@ -1,16 +1,16 @@
 if (Object.const_defined?("Formtastic") && Gem.loaded_specs["formtastic"].version.version[0,1] == "2")
 
-  class RichInput < ::Formtastic::Inputs::TextInput  
+  class RichInput < ::Formtastic::Inputs::TextInput
     def to_html
 
       scope_type = object_name
       scope_id = object.id
       editor_options = Rich.options(options[:config], scope_type, scope_id)
-
+      raise editor_options.to_json.inspect
       input_wrapping do
         label_html <<
         builder.text_area(method, input_html_options) <<
-        "<script>CKEDITOR.replace('#{dom_id}', #{editor_options.to_json.html_safe});</script>".html_safe  
+        "<script>CKEDITOR.replace('#{dom_id}', #{editor_options.to_json.html_safe});</script>".html_safe
       end
     end
   end
